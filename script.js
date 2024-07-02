d20eaef6230b76121a0d04ce62583d4a98a9016a
maxovsee <80675065+maxovsee@users.noreply.github.com>
2024-07-03 00:37:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,25 +10,44 @@ kathismas.forEach((kathisma, index) => {
   }
 });
 
-// Add a button to scroll to the current kathisma, if the user is at the top of the page
+// Add a button to scroll to the current kathisma, if the current kathisma is visible
+function createScrollToKathismaButton() {
+  const currentKathisma = document.querySelector('.kathisma[style*="display: block"]');
+  if (currentKathisma && isElementInViewport(currentKathisma)) {
+    const scrollToKathismaButton = document.createElement('button');
+    scrollToKathismaButton.textContent = 'К Кафизме';
+    scrollToKathismaButton.classList.add('scroll-to-kathisma-button');
+    scrollToKathismaButton.style.position = 'fixed';
+    scrollToKathismaButton.style.bottom = '20px';
+    scrollToKathismaButton.style.right = '20px';
+    scrollToKathismaButton.style.zIndex = '999';
+    scrollToKathismaButton.addEventListener('click', () => {
+      currentKathisma.scrollIntoView({ behavior: 'smooth' });
+    });
+    document.body.appendChild(scrollToKathismaButton);
+  }
+}
+
+// Helper function to check if an element is in the viewport
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Call the createScrollToKathismaButton function on page load
+createScrollToKathismaButton();
+
+// Add a scroll event listener to update the button visibility
 window.addEventListener('scroll', () => {
   const scrollToKathismaButton = document.querySelector('.scroll-to-kathisma-button');
   if (window.pageYOffset <= 100) {
     if (!scrollToKathismaButton) {
-      const newButton = document.createElement('button');
-      newButton.textContent = 'К Кафизме';
-      newButton.classList.add('scroll-to-kathisma-button');
-      newButton.style.position = 'fixed';
-      newButton.style.bottom = '20px';
-      newButton.style.right = '20px';
-      newButton.style.zIndex = '999';
-      newButton.addEventListener('click', () => {
-        const currentKathisma = document.querySelector('.kathisma[style*="display: block"]');
-        if (currentKathisma) {
-          currentKathisma.scrollIntoView({ behavior: 'smooth' });
-        }
-      });
-      document.body.appendChild(newButton);
+      createScrollToKathismaButton();
     }
   } else {
     if (scrollToKathismaButton) {
